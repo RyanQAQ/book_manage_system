0b5184cb44aaf322c28a01f208b946b0f2f8fa55
lyk0915 <1107487574@qq.com>
2020-12-18 09:52:38 +0800

    test

---
+++ library.sql
@@ -0,0 +1,5 @@
+use book_manage_sys;
+
+insert into books values (
+
+);
---
+++ library/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+import datetime
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=32, verbose_name='出版社名字')
+    city = models.CharField(max_length=16, verbose_name='出版社所在城市')
+
+    class Meta:
+        db_table = 'publishers'
+        verbose_name = '出版社'
+        verbose_name_plural = verbose_name
+
+
+class Sort(models.Model):
+    name = models.CharField(max_length=16, verbose_name='分类名')
+
+    class Meta:
+        db_table = 'sort'
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=32, verbose_name='书名')
+    price = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='价格')
+    author = models.CharField(max_length=32, verbose_name='作者')
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, verbose_name='出版社')
+    pub_date = models.DateField(default=datetime.date, verbose_name='出版日期')
+    sort = models.ForeignKey(Sort, on_delete=models.PROTECT, verbose_name='分类')
+
+    class Meta:
+        db_table = 'books'
+        verbose_name = '书籍'
+        verbose_name_plural = verbose_name
